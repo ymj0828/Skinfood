ae51cd4bc0231d574c60ba5c826b1ea7147deadc
ymj0828 <ymj369@nate.com>
2023-03-03 16:37:27 +0900

    js

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,86 @@
+const faqdata = {
+	'상품':{
+		질문:['화장품의 사용기한은 어떻게 되나요?','기초 제품의 사용 순서가 어떻게 되나요?','화장품의 전성분은 어떻게 확인하나요?','매장과 쇼핑몰에서 파는 제품은 동일한가요?'],
+    답변:['제품 개봉 전 사용기한은 제품마다 상이하며 각 제품 하단 유의 사항에 기재되어 있습니다.<br>개봉 후에는 6개월~12개월 이내에 사용해 주시는 것이 가장 좋습니다.<br>단, 사용 기간이 명시되어 있는 제품의 경우 개봉 여부와 관계없이 제품의 유효기간이므로 기간 내 사용하여 주시기 바라며,<br>사용 기한이 남아있더라도 고온 및 저온, 직사광선에 노출되는 장소에서의 보관은 피해주시길 권해드립니다.','스킨푸드의 기초 제품 사용 순서는 토너-아이크림-에센스(세럼)-에멀전-크림 순으로 사용하시면 됩니다.','제품 상세 정보 페이지 하단의 유의사항에서 전성분을 클릭하시면 확인 가능합니다.','매장과 쇼핑몰에서 판매되는 제품은 모두 동일합니다.']
+	},
+	'주문/결제/배송':{
+		질문:['배송일은 얼마나 걸리나요?','온라인에서 주문한 상품을 취소하려면 어떻게 하나요?','배송지를 수정할 수 있나요?'],
+    답변:['상품 발송 후 택배사 영업일 기준 평균 2~4일(주말/ 공휴일/명절연휴 제외) 이내에 배송되며, 제주도 및 도서산간 등 일부 지역은 배송기간이 더 소요될 수 있습니다.<br>(할인 행사 기간내 주문건 증가 시 배송이 다소 지연될 수 있습니다.)','[마이페이지 > 나의 쇼핑 > 주문/배송내역 상세보기]에서 [주문취소] 클릭으로 주문 취소 가능합니다.<br><br>[상품준비]상태까지만 직접 취소 접수 가능하며, 담당자 확인 후 취소 및 환불 처리가 완료 됩니다. 취소 완료 후 사용하신 쿠폰이나 적립금은 자동으로 복원되며, 운영시간 내 처리되는 점 참고 부탁 드립니다. (운영 시간 이후 접수 취소건 익일 영업 재개 후 처리)<br><br>[배송준비] 상태는 출고가 진행된 상태로 담당자 확인 후 출고전인 경우 취소 가능합니다.<br>출고가 완료된 경우 상품 수령 이후 단순 변심으로 인한 반품 신청을 하셔야 하며, 왕복배송비 5,500원을 부담해 주셔야 합니다.','[결제완료/상품준비] 단계의 경우 [마이페이지>나의쇼핑>주문/배송내역>상세보기>배송지정보]에서 [수정하기]를 클릭하여 수정 가능합니다.<br>그러나 [배송준비]중인 경우 출고가 진행된 건으로, 배송지 변경은 불가능하시며, 배송 기사님을 통해 변경 가능합니다.']
+	},
+	'이벤트/쿠폰':{
+    질문:['포인트 소멸기간이 있나요?','매장에서 적립한 포인트는 쇼핑몰에서도 사용 가능한가요?','포인트는 어디에서 확인할 수 있나요?'],
+		답변:['포인트 유효기간은 해당 포인트가 적립된 날로부터 1년 후 소멸됩니다.<br>이메일 수신 허용 고객에 한하여 소멸일 기준으로 약 한달 전 소멸 포인트 안내 이메일이 발송됩니다.','21년 12월 1일부터 온라인 / 오프라인 멤버십 분리 운영으로 온라인 포인트는 온라인에서만 사용이 가능합니다.','[마이페이지]에서 멤버십 등급 확인 및 적립된 포인트를 사용할 수 있습니다.']
+	},
+	'회원정보/멤버십':{
+		질문:['포인트 유효기간은 어떻게 되나요?','회원정보 변경은 어떻게 하나요?','회원 ID와 비밀번호를 분실했는데 어떻게 하죠?'],
+		답변:['포인트 유효기간은 해당 포인트가 적립된 날로부터 1년 후 소멸됩니다.<br>이메일 수신 허용 고객에 한하여 소멸일 기준으로 약 한달 전 소멸 포인트 안내 이메일이 발송됩니다.','[마이페이지>나의계정>회원정보수정]에서 가능합니다.','[아이디/비밀번호 찾기]에서 휴대폰 혹은 아이핀(I-PIN) 본인 인증 과정을 거친 후 확인할 수 있습니다.']
+	},
+	'기타':{
+		질문:['가까운 매장을 찾고 싶어요.'],
+		답변:['[고객선터>매장안내>국내매장 또는 해외매장]에서 원하시는 지역을 선택하시거나<br>매장명을 검색하시면 됩니다.']
+	},
+	'사이트이용':{
+		질문:['위시리스트/ 장바구니에 저장한 상품이 사라졌어요.','사용 후기는 어디서 작성하나요?','온라인 구매 시 선물 포장 가능한가요?'],
+		답변:['장바구니의 경우 7일동안 상품 저장이 되며 장기 보관을 원하실 경우 위시리스트를 이용해 주시기 바랍니다.<br>단, 로그인 후 이용 가능합니다.','상품 후기 작성은<br>각 상품 상세페이지 하단에 [리뷰작성하기] 클릭 후 작성 가능합니다.','쇼핑몰 구매 시 상품의 손상이 없도록 벌집모양 종이 완충제로 또는 에어캡으로 상품별 포장되며 별도의 선물 포장이 불가합니다. ']
+	}
+};
+
 $(() => {
+
+	const board_list = document.querySelector('.board_list');
+	const tab_li = document.querySelectorAll('.tab li')
+
+	// console.log(Object.keys(faqdata))
+	// Object.keys(faqdata).forEach((ele, num)=>{
+		// })
+		// board_list.innerHTML=''
+		tab_li.forEach((tab_li_ele_click)=>{
+			tab_li_ele_click.addEventListener('click', function(){
+
+				if(tab_li_ele_click.classList.contains('active')) return;
+
+				tab_li.forEach((tab_li_ele)=>{
+					tab_li_ele.classList.remove('active')
+				})
+				tab_li_ele_click.classList.add('active')
+
+				board_list.innerHTML=''
+				
+				faqdata[tab_li_ele_click.innerText].질문.forEach((faqdata_ele, faqdata_num)=>{
+
+					board_list.innerHTML+=`<li>
+					<div class="board_title"><span class="board_cat">[${tab_li_ele_click.innerText}]</span> ${faqdata[tab_li_ele_click.innerText].질문[faqdata_num]}
+						<div class="board_answer">${faqdata[tab_li_ele_click.innerText].답변[faqdata_num]}
+						</div>
+					</div>
+					<div class="board_btn"></div>
+				</li>`
+
+				})
+				boardSlide()
+				
+			})
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	const gnb = document.querySelector('.gnb')
 	const gnb_2depth = document.querySelectorAll('.gnb_2depth')
@@ -69,6 +151,8 @@ $(() => {
 
 
 
+function boardSlide(){
+
 	const board_li = $('.board_list li')
 	const board_answer = $('.board_answer')
 
@@ -79,7 +163,7 @@ $(() => {
 			board_answer.each((num, board_answer_ele)=>{
 				$(board_answer_ele).slideUp(300)
 			})
-			console.log($(board_answer[board_li_num_click]).css('display') == 'block')
+
 			if($(board_answer[board_li_num_click]).css('display') == 'block') return
 
 			$(board_answer[board_li_num_click]).slideDown(300)
@@ -89,7 +173,7 @@ $(() => {
 
 	})
 
-
+}
 
 
 
